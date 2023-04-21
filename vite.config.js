5e69164445c395a7acf2143dd29f90975d322e8e
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2023-04-21 18:09:18 +0100

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-  
-})
   module.exports = {
     server: {
       port: 3000,
